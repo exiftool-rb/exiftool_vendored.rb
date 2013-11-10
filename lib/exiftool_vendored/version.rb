30000670239128a3f9983fce79a852adee63c7ab
Matthew McEachen <matthew+github@mceachen.org>
2013-11-09 22:01:30 -0800

    Fixed version parsing to handle "9.40"

--- lib/exiftool_vendored/version.rb
+++ lib/exiftool_vendored/version.rb
@@ -1,3 +1,3 @@
 module ExiftoolVendored
-  VERSION = Gem::Version.new('9.4.0')
+  VERSION = Gem::Version.new('9.40.0')
 end
--- lib/exiftool_vendored/version_extractor.rb
+++ lib/exiftool_vendored/version_extractor.rb
@@ -3,12 +3,13 @@ module ExiftoolVendored
 
   # This is only used by the rake 'update_exiftool' task.
   def self.extract_version
-    require 'yaml'
-    meta_yml = Dir[File.expand_path("../../../bin/*/META.yml", __FILE__)].first
-    return 'unknown' unless meta_yml && File.exist?(meta_yml)
-    version = YAML::load_file(meta_yml)['version']
+    # We can't use the YAML file, because he doesn't encode it as a string,
+    # so "9.40" returns as "9.4"
+    dirname = Dir[File.expand_path("../../../bin/Image-ExifTool-*", __FILE__)].sort.last
+    basename = File.basename(dirname)
+    version = basename["Image-ExifTool-".length..-1]
     unless version.to_f > 0
-      raise "version is missing from #{meta_yml}"
+      raise "version is missing from #{dirname}"
     end
     "#{version}.#{PATCHLEVEL}"
   rescue StandardError => e
