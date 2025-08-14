bdb23ed05e76801e9102c5762917d0d9d5efa0d1
morozgrafix <morozgrafix@gmail.com>
2025-08-13 22:56:14 -0700

    fix rubocop linting

--- test/exiftool_command_setup_test.rb
+++ test/exiftool_command_setup_test.rb
@@ -6,12 +6,14 @@ describe ExiftoolVendored do
   it 'sets up Exiftool to work with the vendored command' do
     bin = File.expand_path('../bin', __dir__)
     expected_path = Regexp.new("#{bin}/exiftool")
+
     value(Exiftool.command).must_match expected_path
   end
 
   it 'sets up path to Exiftool home for vendored command' do
     bin = File.expand_path('../bin', __dir__)
     expected_path = Regexp.new(bin.to_s)
+
     value(ExiftoolVendored.path_to_exiftool_home).must_match expected_path
   end
 
